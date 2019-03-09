283faef41de43284f9f7cefeac76ab05e5ed265b
Juha Kaskiharju <juha.kaskiharju@gmail.com>
2019-03-10 00:09:02 +0200

    illan vika commit

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ minecraftpricesApp.controller("hinnat", function ($scope, $http) {
   $http.get("items.json").then(function(response) {
       $scope.hinnat = response.data.records;
   });
+  
+  $scope.propertyName = 'id';
+  $scope.reverse = false;
+ 
+  $scope.sortBy = function(propertyName) {
+    $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+    $scope.propertyName = propertyName;
+  };
 });
